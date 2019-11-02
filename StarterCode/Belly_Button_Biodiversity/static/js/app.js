98de963141ffbdbde460983f329d49a27141efc3
Wilmorecn <wilmorecn@gmail.com>
2019-11-02 15:10:37 -0500

    push

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,16 +1,17 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-
+  var url = "/metadata/<sample>";
+  d3.json(url).then(function(sample) {
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    var li1 = d3.select("ul").append("li");
     // Use `.html("") to clear any existing metadata
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+  }
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
