54b3737c3dc99b72a18256bab625aab3f9e6e6c2
Wilmorecn <wilmorecn@gmail.com>
2019-11-06 18:31:37 -0600

    commit 3

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,31 +1,71 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-  var url = "/metadata/<sample>";
-  d3.json(url).then(function(sample) {
   // Use `d3.json` to fetch the metadata for a sample
+  // var url = "/metadata/<sample>";
+  d3.json(`/metadata/${sample}`).then(data => {
     // Use d3 to select the panel with id of `#sample-metadata`
-    var li1 = d3.select("ul").append("li");
+    var Metadata = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+    Metadata.html("");
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-  }
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+    // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      // console.log(key, value);
+      Metadata.append("p").text(`${key}: ${value}`);
+    });
+  });
+
+  // BONUS: Build the Gauge Chart
+  // buildGauge(data.WFREQ);
+};
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  d3.json(`/samples/${sample}`).then((response) => {
+    // var otu_ids = response.otu_ids;
+    // var otu_labels = response.otu_labels;
+    // var sample_values = data.sample_values;
     // @TODO: Build a Bubble Chart using the sample data
-
+    var trace1 = {
+      x: response.otu_ids,
+      y: response.sample_values,
+      mode: 'markers',
+      marker: {
+        size: response.sample_values,
+        // sizeref = 2.0 * Math.max(...size) / (40 ** 2),
+        sizemode: 'area'
+      },
+      type: 'scatter'
+    };
+    var data = [trace1];
+    var layout = {
+      title: 'Belly Button Diversity',
+      showlegend: false,
+      height: 600,
+      width: 1000,
+      // margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Sample Volume" }
+    };
+    Plotly.newPlot('bubble', data, layout);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+    var data = [{
+      values: response.sample_values.slice(0, 10),
+      labels: response.otu_ids.slice(0, 10),
+      type: 'pie'
+    }];
+    var layout = {
+      height: 400,
+      width: 500
+    };
+    Plotly.newPlot('pie', data, layout);
+  });
+};
 
 function init() {
   // Grab a reference to the dropdown select element
